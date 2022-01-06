4a761dacccf70f6f12d3bc0c0d8988707654a72f
Carlos Zimmerle <carloszimmerle@gmail.com>
2022-01-05 22:03:08 -0300

    refactor: move sleep handling to a function and add search limit verification

--- internal/error-handling/worker.go
+++ internal/error-handling/worker.go
@@ -2,7 +2,6 @@ package errorhandling
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"time"
 
@@ -13,18 +12,20 @@ func HandleErrorWorkers(err error, id int, resp *github.Response, client *github
 	ctx := context.Background()
 
 	if _, ok := err.(*github.RateLimitError); ok {
-		d := time.Until(resp.Rate.Reset.Time)
-		log.Println("worker", id, "went to sleep for", fmt.Sprint(d.Minutes()), "minutes")
-		time.Sleep(d)
+		handleSleep(id, time.Until(resp.Rate.Reset.Time))
 	} else {
 		// checks the Rate Limiting API in case the above doesn't work properly
 		rateLimit, _, errRLimit := client.RateLimits(ctx)
 		if err == nil {
-			coreLimit := rateLimit.GetCore()
-			if coreLimit.Remaining == 0 {
-				d := time.Until(coreLimit.Reset.Time)
-				log.Println("worker", id, "went to sleep for", fmt.Sprint(d.Minutes()), "minutes")
-				time.Sleep(d)
+			limit := rateLimit.GetCore()
+			if limit.Remaining == 0 {
+				handleSleep(id, time.Until(limit.Reset.Time))
+			} else {
+				// search API has a different limit
+				limit = rateLimit.GetSearch()
+				if limit.Remaining == 0 {
+					handleSleep(id, time.Until(limit.Reset.Time))
+				}
 			}
 		} else {
 			// log errors
@@ -32,4 +33,9 @@ func HandleErrorWorkers(err error, id int, resp *github.Response, client *github
 			log.Println(errRLimit)
 		}
 	}
+}
+
+func handleSleep(id int, d time.Duration) {
+	log.Printf("worker %d went to sleep for %f minutes", id, d.Minutes())
+	time.Sleep(d)
 }
